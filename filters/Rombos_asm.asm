6415114a06a803f8da67557fc2f1bf2bf6001fc0
fran <fciarliero@dc.uba.ar>
2019-09-24 16:30:04 -0300

    falta terminar

--- filters/Rombos_asm.asm
+++ filters/Rombos_asm.asm
@@ -41,50 +41,67 @@ movdqu xmm5, [pfullsize]	;todos los componentes de xmm5 son 64
 movdqu xmm9, [casobordesuperior]	;primeros 4 componentes 32, resto 0
 
 pxor xmm3, xmm3 
-
+pxor xmm4, xmm4
+mov r10, r15
+dec r10
+movdqu xmm4, xmm2
 .altura:
 	cmp r12d, ecx
 	je .fin 
 	xor r13, r13 		;anchura de nueva fila empieza de cero
 	cmp r10, r14 		;veo si contador altura % 64 > 0
-	jge .decii
-	.incii:
-		movdqu xmm4, xmm2 	;xmm4 = |32|32|32|32|
-		psubw xmm4, xmm3	;xmm4 = |32 - (altura % 64)|..|..|32 - (altura % 64)|
-		paddw xmm3, xmm0 	;xmm3 = |altura % 64 + 1|..|..|altura % 64 +1|
-		jmp .anchura
-	.decii:
-		cmp r10, r8 		;veo si contador resto = 128
-		jne .decii_1
-			xor r10, r10
-			jmp .incii
-		.decii_1:
-		movdqu xmm4, xmm2 	;xmm4 = |32|32|32|32|
-		psubw xmm3, xmm0 	;xmm3 = |altura % 64 - 1|..|..|altura % 64 -1|
-		psubw xmm4, xmm3	;xmm4 = |32 - (altura % 64)|..|..|32 - (altura % 64)|
+	je .cont0
+	cmp r10, r15		;cont < 32?
+	jge .decRest
+	paddw xmm4, xmm0
+	jmp .cont
+	.decRest:
+	psubw xmm4, xmm0
+	jmp .cont
+	.cont0:
+	xor r10, r10
+	jmp .altura
+	.cont:
+	;psubw xmm4, xmm2
+	pabsw xmm4, xmm4 
+	;jge .decii
+	;.incii:
+	;	movdqu xmm4, xmm2 	;xmm4 = |32|32|32|32|
+	;	psubw xmm4, xmm3	;xmm4 = |32 - (altura % 64)|..|..|32 - (altura % 64)|
+	;	paddw xmm3, xmm0 	;xmm3 = |altura % 64 + 1|..|..|altura % 64 +1|
+	;	jmp .anchura
+	;.decii:
+	;	cmp r10, r8 		;veo si contador resto = 128
+	;	jne .decii_1
+	;		xor r10, r10
+	;		jmp .incii
+	;	.decii_1:
+	;	movdqu xmm4, xmm2 	;xmm4 = |32|32|32|32|
+	;	psubw xmm3, xmm0 	;xmm3 = |altura % 64 - 1|..|..|altura % 64 -1|
+	;	psubw xmm4, xmm3	;xmm4 = |32 - (altura % 64)|..|..|32 - (altura % 64)|
 	;hasta aqui xmm4 = ii
 	.anchura:	
-		cmp r11, r14		;veo si el contador de anchura % 64 >= 63
-		;calculo operador jj = ((size/2)-(j%size)) > 0 ? ((size/2)-(j%size)) : -((size/2)-(j%size));
-			;xmm6 va a ser jj
-			.prmEtapa:			;aca pongo el contador del primer pixel en 0, incremento el del segundo que estaba en cero por segetapa
-			cmp r11, NULL		;veo si el contador de anchura == 0
-			jne .segEtapa
-			movdqu xmm6, xmm1	;xmm6 = |0|0|0|0|1|1|1|1|
-			jmp .continua
-			.segEtapa:			;aca me fijo si el contador/resto 64 llega a 62, cuyo caso pongo el contador del 2 pixel en 63
-			inc r11
-			cmp r11, r14 		;contador + 1 < 63 ? calculo jj normalmente : pongo 63 contador segundo pixel, 64 el primero
-			dec r11				;retorno r11 valor orish. dec no afecta flags (?methinks)
-			jne .terEtapa		;calculo jj normalmente
-			movdqu xmm6, xmm9 	;xmm6 = |64|64|64|64|63|63|63|63|
-			xor r11, r11 		; reset del contador / resto
-			jmp .continua
-			.terEtapa:			;en esta parte me fijo si tengo que decrementar o incrementar el contador/resto
-			cmp r11, r15 		;contador/resto <= 32? incremento : decremento
-			jge .decr
-			.incr:
-			paddw xmm6, xmm0
+		;cmp r11, r14		;veo si el contador de anchura % 64 >= 63
+		;;calculo operador jj = ((size/2)-(j%size)) > 0 ? ((size/2)-(j%size)) : -((size/2)-(j%size));
+		;	;xmm6 va a ser jj
+		;	.prmEtapa:			;aca pongo el contador del primer pixel en 0, incremento el del segundo que estaba en cero por segetapa
+		;	cmp r11, NULL		;veo si el contador de anchura == 0
+		;	jne .segEtapa
+		;	movdqu xmm6, xmm1	;xmm6 = |0|0|0|0|1|1|1|1|
+		;	jmp .continua
+		;	.segEtapa:			;aca me fijo si el contador/resto 64 llega a 62, cuyo caso pongo el contador del 2 pixel en 63
+		;	inc r11
+		;	cmp r11, r14 		;contador + 1 < 63 ? calculo jj normalmente : pongo 63 contador segundo pixel, 64 el primero
+		;	dec r11				;retorno r11 valor orish. dec no afecta flags (?methinks)
+		;	jne .terEtapa		;calculo jj normalmente
+		;	movdqu xmm6, xmm9 	;xmm6 = |64|64|64|64|63|63|63|63|
+		;	xor r11, r11 		; reset del contador / resto
+		;	jmp .continua
+		;	.terEtapa:			;en esta parte me fijo si tengo que decrementar o incrementar el contador/resto
+		;	cmp r11, r15 		;contador/resto <= 32? incremento : decremento
+		;	jge .decr
+		;	.incr:
+		;	paddw xmm6, xmm0
 
 
 
@@ -93,7 +110,7 @@ pxor xmm3, xmm3
 
 		.continua:
 		;calculo x = ii+jj-(size/2)) > (size/16) ? 0 : 2*(ii+jj-(size/2))
-		pabsw xmm4, xmm4 	;xmm4 = |abs(32 - (altura % 64))|..|..|abs(32 - (altura % 64))|
+			;xmm4 = |abs(32 - (altura % 64))|..|..|abs(32 - (altura % 64))|
 		movdqu xmm6, xmm4 	;xmm6 = xmm4 = ii
 		psubw xmm6, xmm2 	;xmm6 = ii - 32
 		movdqu xmm7, xmm5 	;xmm7 = xmm5 = 64
